1fd84e2a4281175105ac271e001d91c4940ba3b7
Pranav Anne <pranav.anne21@gmail.com>
2024-11-09 12:40:13 -0500

    handle user input edge cases.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
 
   const [inputData, setInputData] = useState({initialInvestment: 10000, annualInvestment: 1200, expectedReturn: 6, duration: 10});
 
+  const isInputValid = inputData.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
       setInputData((prevInputData) => {
           return {...prevInputData, [inputIdentifier]: +newValue};
@@ -17,7 +19,7 @@ function App() {
     <>
       <Header></Header>
       <UserInput inputData={inputData} handleChange={handleChange}></UserInput>
-      <Results inputData={inputData}></Results>
+      {isInputValid ? <Results inputData={inputData}></Results>: <p className="center">Please enter valid duration.</p>}
     </>
   )
 }
